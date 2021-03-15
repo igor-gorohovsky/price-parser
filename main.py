ee56570566ef8bb0943965d9ddd3763db5d43b25
igor-gorohovsky <igor.gorohhovsky@yandex.ru>
2021-03-15 17:40:45 +0200

    Rewrite 5881747 commit with OOP

--- main.py
+++ main.py
@@ -2,32 +2,18 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 
-driver = ""
+class ChromeBrowser():
 
-
-# Open browser in headless mode
-def set_up():
-	try:
+	def __init__(self):
 		options = Options()
 		options.add_argument("--headless")
-		global driver
-		driver = webdriver.Chrome(options=options)
-	except Exception as e:
-		driver.quit()
-		print(e)
+
+		try:			
+			self.driver = webdriver.Chrome(options=options)
+		except Exception as e:
+			self.driver.quit()
+			print(e)
 
 
-# Close the browser after working
-def tear_down():
-	driver.quit()	
-
-
-def main():
-	set_up()
-	page = driver.get("http://www.google.com")
-	assert "Google" in driver.title
-	tear_down()
-
-
-if __name__ == '__main__':
-	main()
+	def tear_down(self):
+		self.driver.quit()
