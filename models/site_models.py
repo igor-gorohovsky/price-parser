267fad8903fce26c8012b7e54fea8de265db6762
igor-gorohovsky <igor.gorohhovsky@yandex.ru>
2021-03-16 17:11:46 +0200

    Add data models by sqlalchemy

---
+++ models/site_models.py
@@ -0,0 +1,27 @@
+from sqlalchemy import *
+from sqlalchemy.orm import declarative_base
+
+engine = create_engine('postgresql+psycopg2:///parser', echo=True)
+
+
+Base = declarative_base()
+
+
+class Urls(Base):
+	__tablename__ = 'urls'
+
+	id = Column(Integer, primary_key=True)
+	url = Column(String, nullable=False)
+
+
+class Prices(Base):
+	__tablename__ = 'prices'
+	__table_args__ = (PrimaryKeyConstraint('url_id', 'date'), {})
+
+	url_id = Column(Integer, ForeignKey('urls.id'))
+	date = Column(DateTime, nullable=False)
+	price = Column(String(10))
+
+
+if __name__ == '__main__':
+	Base.metadata.create_all(engine)
