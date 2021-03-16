8d893a8cb817f7e45d375f9413708dea53ebbedc
igor-gorohovsky <igor.gorohhovsky@yandex.ru>
2021-03-16 23:10:03 +0200

    Add comments

--- add_urls_from_txt_file.py
+++ add_urls_from_txt_file.py
@@ -5,6 +5,28 @@ from models.site_models import *
 from models.db_config import config
 
 
+def get_urls_from_txt():
+	
+	"""Read urls to add them to the DB"""
+
+	urls = []
+	with open("urls.txt") as file:
+		for line in file.readlines():
+			urls.append(line)			
+	return urls
+
+
+def get_list_for_query(urls):
+
+	""" Create a list of instances of the Urls class.
+	It need to add all urls to DB with one request. """
+
+	instances = []
+	for url in urls:
+		instances.append(Urls(url=url))
+	return instances
+
+
 def main():
 	engine = engine_from_config(config, prefix='db.')
 
@@ -17,20 +39,6 @@ def main():
 	session.add_all(instances)
 	session.commit()	
 
-
-def _get_urls_from_txt():
-	urls = []
-	with open("urls.txt") as file:
-		for line in file.readlines():
-			urls.append(line)			
-	return urls
-
-
-def _create_list_of_instances_of_the_mapped_class(urls):
-	instances = []
-	for url in urls:
-		instances.append(Urls(url=url))
-	return instances
 
 if __name__ == '__main__':
 	main()
