fc76ba9954a69cba88d4cd52f0e4418f8f313c9e
igor-gorohovsky <igor.gorohhovsky@yandex.ru>
2021-03-31 14:46:29 +0300

    Remove secret settings

--- price_parser/price_parser/settings.py
+++ price_parser/price_parser/settings.py
@@ -10,20 +10,26 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import environ
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+environ.Env.read_env()
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a%*w1=p)1snzuxte_l^^5)(+g684ylddhp7kdbhxdj1lcl&p_+'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
