189b73f99aa394fd5655b049b658c3d530d00808
igor-gorohovsky <igor.gorohhovsky@yandex.ru>
2021-04-01 17:12:48 +0300

    Improve admin view

--- price_parser/parser_app/admin.py
+++ price_parser/parser_app/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from .models import Urls, Prices
 
-# Register your models here.
-admin.site.register(Urls)
-admin.site.register(Prices)
+
+class UrlsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'url')
+    list_display_links = ('id', 'url')
+
+
+class PricesAdmin(admin.ModelAdmin):
+    list_display = ('url_id', 'date', 'current_price', 'status')
+    search_fields = ('current_price', 'status')
+
+
+admin.site.register(Urls, UrlsAdmin)
+admin.site.register(Prices, PricesAdmin)
