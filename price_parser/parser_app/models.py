73d5538f1a89a44ebb08e4b2f39788805b51ff8e
igor-gorohovsky <igor.gorohhovsky@yandex.ru>
2021-04-02 23:00:15 +0300

    Display images of the products

--- price_parser/parser_app/models.py
+++ price_parser/parser_app/models.py
@@ -6,6 +6,7 @@
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
+from django.conf import settings
 
 
 class Prices(models.Model):
@@ -28,12 +29,15 @@ class Prices(models.Model):
 class Urls(models.Model):
 
     url = models.TextField(unique=True)
+    product_photo = models.ImageField(
+        upload_to='product',
+        default='default.png'
+    )
 
     def __str__(self):
         return f'{self.id}: {self.url}'
 
     class Meta:
-        managed = False
         db_table = 'urls'
         verbose_name='URL'
         verbose_name_plural='URLs'
--- price_parser/price_parser/settings.py
+++ price_parser/price_parser/settings.py
@@ -130,3 +130,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Media files
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
