b78fd4d0421e37cf5fdf003cff85ab6350a919ee
igor-gorohovsky <igor.gorohhovsky@yandex.ru>
2021-04-05 18:57:00 +0300

    Change type of url field

--- price_parser/parser_app/models.py
+++ price_parser/parser_app/models.py
@@ -29,7 +29,7 @@ class Prices(models.Model):
 
 class Urls(models.Model):
 
-    url = models.TextField(unique=True)
+    url = models.URLField(unique=True)
     name = models.CharField(max_length=255, default='Undefiend name')
     product_photo = models.ImageField(
         upload_to='product',
